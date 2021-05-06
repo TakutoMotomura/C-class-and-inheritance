9c6fe9829bf0085da68b459f2eb048ef16b343b8
TakutoMotomura <takumoto913@gmail.com>
2021-05-06 23:52:23 +0900

    20210506

---
+++ chapter6/List6-7.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+using namespace std;
+
+class Figure
+{
+    public:
+        virtual void draw();
+};
+
+void Figure::draw()
+{
+    // ˆ—‚È‚µ
+}
+
+
+class Circle : public Figure
+{
+    public:
+        void draw();
+};
+
+void Circle::draw()
+{
+    cout << "Z";
+}
+
+
+class Triangle : public Figure
+{
+    public:
+        void draw();
+};
+
+void Triangle::draw()
+{
+    cout << "¢";
+}
+
+class Rectangle : public Figure
+{
+    public:
+        void draw();
+};
+
+void Rectangle::draw()
+{
+    cout << " ";
+}
+
+
+
+
+int main()
+{
+    Figure *p[100];
+
+    Circle c1;
+    p[0] = &c1;
+
+    Triangle t1,t2;
+    p[1] = &t1;
+    p[2] = &t2;
+
+    Rectangle r1,r2,r3;
+    p[3] = &r1;
+    p[4] = &r2;
+    p[5] = &r3;
+
+    p[6] = NULL;
+
+
+    int i;
+    
+    for( i = 0; p[i] != NULL; i++ )
+        {
+            p[i]->draw();
+        }
+    
+    cout << "\n";
+
+
+    // æ“ª‚©‚ç3”Ô–Ú‚Ì}Œ`‚ğíœ‚·‚é
+    for( i = 2; i < 5; i++ )
+        {
+            p[i] = p[i+1];
+        }
+
+    p[i] = NULL;
+
+
+    // }Œ`‚ğÄ•`Ê
+    for( i = 0; p[i] != NULL; i++ )
+        {
+            p[i]->draw();
+        }
+    
+    cout << "\n";
+
+    p[0]->draw();
+    cout << "\n";
+    p[1]->draw();
+    cout << "\n";
+    p[2]->draw();
+    cout << "\n";
+    p[3]->draw();
+    cout << "\n";
+    p[4]->draw();
+    cout << "\n";
+
+    return 0;
+}
