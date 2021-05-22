2d629653d7a5952161a4e282805844aec4bb8f01
TakutoMotomura <takumoto913@gmail.com>
2021-05-23 01:26:38 +0900

    20210523

---
+++ chapter9/List9-8.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class YourClass;
+
+class MyClass
+{
+    private:
+        int val;
+
+        friend void compObj( MyClass *m, YourClass *y );
+    public:
+        MyClass()
+        {
+            val = 0;
+        }
+
+        MyClass( int v )
+        {
+            val = v;
+        }
+
+};
+
+class YourClass
+{
+    private:
+        int num;
+
+        friend void compObj( MyClass *m, YourClass *y );
+    
+    public:
+        YourClass()
+        {
+            num = 0;
+        }
+
+        YourClass( int n )
+        {
+            num = n;
+        }
+};
+
+void compObj( MyClass *m, YourClass *y )
+{
+    if( m->val > y->num )
+    {
+        cout << "val ‚Í num ‚æ‚è‘å‚«‚¢\n";
+    }
+    else if( m->val < y->num )
+    {
+        cout << "val ‚Í num ‚æ‚è¬‚³‚¢\n";
+    }
+    else
+    {
+        cout << "val ‚Æ num ‚Í“™‚µ‚¢\n";
+    }
+}
+
+int main()
+{
+    MyClass mc( 123 );
+    YourClass yc( 456 );
+
+    compObj( &mc, &yc );
+
+    return 0;
+}
