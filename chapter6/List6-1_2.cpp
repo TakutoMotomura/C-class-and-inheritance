529c0d91c52275e09225affee06a53e7791b391f
TakutoMotomura <takumoto913@gmail.com>
2021-06-06 00:29:05 +0900

    20210606

---
+++ chapter6/List6-1_2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class MyClass
+{
+    public:
+        void func1( int a );
+        virtual void func2( const char *s );
+};
+
+void MyClass::func1( int a )
+{
+    cout << a << "\n";
+}
+
+void MyClass::func2( const char *s )
+{
+    cout << s << "\n";
+}
+
+class NewClass:public MyClass
+{
+    public:
+        void func2( const char *s );
+};
+
+void NewClass::func2( const char *s )
+{
+    cout << "文字列データ：";
+    cout << s << "\n";
+}
+
+
+int main()
+{
+    NewClass obj;
+
+    obj.func1( 123 );
+
+    obj.func2( "こんにちわ" );
+
+    return 0;
+}
