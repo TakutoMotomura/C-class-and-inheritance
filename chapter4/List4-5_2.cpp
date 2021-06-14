f4807b470188fa71193e269ddfd984db2bce37df
TakutoMotomura <takumoto913@gmail.com>
2021-06-14 23:38:24 +0900

    20210614

---
+++ chapter4/List4-5_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class MyClass
+{
+    public:
+        int number;
+        MyClass();
+        ~MyClass();
+};
+
+MyClass::MyClass()
+{
+    cout << "コンストラクタが呼び出されたよ\n";
+}
+
+MyClass::~MyClass()
+{
+    cout << "オブジェクト" << number;
+    cout << "のデストラクタが呼び出されたよ\n";
+}
+
+
+void myFunc()
+{
+    MyClass obj1;
+    obj1.number = 1;
+}
+
+int main()
+{
+    myFunc();
+    MyClass obj2;
+    obj2.number = 2;
+
+    return 0;
+}
