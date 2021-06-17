1d6b790406a67c8db761e22711fc37e1482e20e9
TakutoMotomura <takumoto913@gmail.com>
2021-06-17 23:29:58 +0900

    20210617

---
+++ chapter7/Employee_2.h
@@ -0,0 +1,10 @@
+class Employee
+{
+    public:
+        static const char *companyName;
+        int number;
+        char name[80];
+        int salary;
+};
+
+const char *Employee::companyName = "¬¼»ìŠ";
---
+++ chapter7/List7-5_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Employee_2.h"
+using namespace std;
+
+int main()
+{
+    cout << Employee::companyName << "\n";
+
+    Employee tanaka;
+
+    cout << tanaka.companyName << "\n";
+
+    return 0;
+}
