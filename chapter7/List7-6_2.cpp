86647f081f98eb420cf8508fd3c7e55d68d3aaf3
TakutoMotomura <takumoto913@gmail.com>
2021-06-19 01:17:31 +0900

    20210618

---
+++ chapter7/List7-6_2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Employee
+{
+    public:
+        static int objNum;
+        static const char *companyName;
+        int number;
+        char name[80];
+        int salary;
+        void showData();
+        Employee();
+        Employee( const char *na, int sa );
+};
+
+int Employee::objNum = 0;
+const char *Employee::companyName = "小松製作所";
+
+Employee::Employee()
+{
+    objNum++;
+    number = objNum;
+    strcpy( name, "未設定" );
+    salary = 150000;
+}
+
+Employee::Employee( const char *na, int sa )
+{
+    objNum++;
+    number = objNum;
+    strcpy( name, na );
+    salary = sa;
+}
+
+void Employee::showData()
+{
+    cout << "会社名：" << companyName << "\n";
+    cout << "社員番号：" << number << "\n";
+    cout << "氏名：" << name << "\n";
+    cout << "給与：" << salary << "\n";
+}
+
+int main()
+{
+    Employee tanaka( "田中一郎", 200000 );
+    Employee sato( "佐藤二郎" , 250000 );
+    Employee someone;
+
+    tanaka.showData();
+    sato.showData();
+    someone.showData();
+
+    cout << "現在の社員数：" << Employee::objNum << "\n";
+
+    return 0;
+}
